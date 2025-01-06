d7b74011f2655f75fde32ced29f4cc555f6132a0
Hensell <Hensell04@hotmail.com>
2025-01-06 12:06:30 -0600

    Actualizar page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import MainLayout from "./layouts/MainLayout";
 export default function Home() {
   return (
     <MainLayout>
-      <section id="about" className="min-h-[100dvh]  bg-slate-600 scroll-mt-[80px]">
+      <section id="about" className="min-h-[100vh]  bg-slate-600 scroll-mt-[80px]">
       <h1 className="text-[clamp(1.8rem,calc(7vw+1rem),5rem)] text-black">Hensell </h1>
       </section>
-      <section id="projects" className="min-h-[100dvh] bg-red-500 scroll-mt-[80px]">
+      <section id="projects" className="min-h-[100vh] bg-red-500 scroll-mt-[80px]">
         Section 2
       </section>
-      <section id="contact" className="min-h-[100dvh] bg-cyan-500 scroll-mt-[80px]">
+      <section id="contact" className="min-h-[100vh] bg-cyan-500 scroll-mt-[80px]">
         Section 3
       </section>
     </MainLayout>
