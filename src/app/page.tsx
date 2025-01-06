25a5f2c17052d1536e1ff8fdcd65cf7f3bb90df4
Hensell <hensell04@hotmail.com>
2025-01-05 20:20:27 -0600

    Update main padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <MainLayout>
       <section id="section-1" className="min-h-[100dvh] pt-10">
-      <h1 className="text-[clamp(1.8rem,calc(7vw+1rem),5rem)] text-black">Hensell </h1>
+      <h1 className="text-[clamp(1.8rem,calc(7vw+1rem),5rem)] text-black scroll-mt-[80px]">Hensell </h1>
       </section>
-      <section id="projects" className="min-h-[100dvh] bg-red-500 pt-16 ">
+      <section id="projects" className="min-h-[100dvh] bg-red-500 scroll-mt-[80px] ">
         Section 2
       </section>
-      <section id="contact" className="min-h-[100dvh] bg-cyan-500 pt-16">
+      <section id="contact" className="min-h-[100dvh] bg-cyan-500 scroll-mt-[80px]">
         Section 3
       </section>
     </MainLayout>
