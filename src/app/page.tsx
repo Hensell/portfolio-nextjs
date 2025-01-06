9a86920356098eb9376837e53e8712711f6742f1
Hensell <hensell04@hotmail.com>
2025-01-05 20:33:24 -0600

    Update main padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import MainLayout from "./layouts/MainLayout";
 export default function Home() {
   return (
     <MainLayout>
-      <section id="about" className="min-h-[100dvh] pt-10">
-      <h1 className="text-[clamp(1.8rem,calc(7vw+1rem),5rem)] text-black scroll-mt-[80px]">Hensell </h1>
+      <section id="about" className="min-h-[100dvh]  bg-slate-600 scroll-mt-[80px]">
+      <h1 className="text-[clamp(1.8rem,calc(7vw+1rem),5rem)] text-black">Hensell </h1>
       </section>
-      <section id="projects" className="min-h-[100dvh] bg-red-500 scroll-mt-[80px] ">
+      <section id="projects" className="min-h-[100dvh] bg-red-500 scroll-mt-[80px]">
         Section 2
       </section>
       <section id="contact" className="min-h-[100dvh] bg-cyan-500 scroll-mt-[80px]">
