8df32522cfd5c10f887081cba7b84944a0be0bfe
Hensell <Hensell04@hotmail.com>
2025-01-08 22:36:37 -0600

    Actualizar page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   id="about"
   className="scroll-mt-[80px] flex flex-col md:flex-row gap-4"
 >
-  <div className="bg-[#EDEBE6] p-5 md:p-16 rounded-3xl shadow-lg mr-5">
+  <div className="bg-[#EDEBE6] p-5 md:p-16 rounded-3xl shadow-lg mr-0 md:mr-5">
     <h1 className="text-[clamp(1rem,calc(5vw+0.8rem),2.5rem)] text-[#1C1C1C] text-center md:text-left">
       <span className="font-black">Hensell </span>
       is a Full Stack Developer creating seamless web and mobile experiences with modern technologies
