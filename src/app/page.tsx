d83fe80fd251abdd9ac404313e968e87f0b0917c
Hensell <hensell04@hotmail.com>
2025-01-08 22:03:46 -0600

    Added h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,34 @@ import MainLayout from "./layouts/MainLayout";
 export default function Home() {
   return (
     <MainLayout>
-      <section id="about" className="min-h-[100vh]  bg-slate-600 scroll-mt-[80px]">
-      <h1 className="text-[clamp(1.8rem,calc(7vw+1rem),5rem)] text-black">Hensell </h1>
-      </section>
-      <section id="projects" className="min-h-[100vh] bg-red-500 scroll-mt-[80px]">
+ <section
+  id="about"
+  className="scroll-mt-[80px] flex flex-col md:flex-row gap-4"
+>
+  <div className="bg-[#EDEBE6] p-5 md:p-16 rounded-3xl shadow-lg mr-5">
+    <h1 className="text-[clamp(1rem,calc(5vw+0.8rem),2.5rem)] text-[#1C1C1C] text-center md:text-left">
+      <span className="font-black">Hensell </span>
+      is a Full Stack Developer creating seamless web and mobile experiences with modern technologies
+    </h1>
+  </div>
+
+  <div>
+    <p className="text-black break-words">
+      Experienceeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    </p>
+  </div>
+</section>
+
+      <section
+        id="projects"
+        className="min-h-[100vh] bg-red-500 scroll-mt-[80px]"
+      >
         Section 2
       </section>
-      <section id="contact" className="min-h-[100vh] bg-cyan-500 scroll-mt-[80px]">
+      <section
+        id="contact"
+        className="min-h-[100vh] bg-cyan-500 scroll-mt-[80px]"
+      >
         Section 3
       </section>
     </MainLayout>
