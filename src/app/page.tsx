f0bc4cd234275fa98835c9ec172fb0b430c59604
Hensell <hensell04@hotmail.com>
2025-01-10 23:49:16 -0600

    Bento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,19 @@ export default function Home() {
     <MainLayout>
       <section
         id="about"
-        className="scroll-mt-[80px] flex flex-col md:flex-row gap-4"
+        className="scroll-mt-[80px] gap-4 grid grid-cols-1 md:grid-cols-3"
       >
-        <div className="bg-[#EDEBE6] p-5 md:p-16 rounded-3xl shadow-lg mr-0 md:mr-5">
-          <h1 className="text-[clamp(1rem,calc(5vw+0.8rem),2.5rem)] text-[#1C1C1C] text-center md:text-left">
-            <span className="font-black">Hensell </span>
-            is a Full Stack Developer creating seamless web and mobile
-            experiences with modern technologies
-          </h1>
+        <div className="mr-0 md:mr-5 md:col-span-2 md:row-span-1">
+          <div className="p-5 md:p-16 bg-[#EDEBE6] shadow-lg mb-8 rounded-3xl">
+            <h1 className="text-[clamp(1rem,calc(5vw+0.8rem),2.5rem)] text-[#1C1C1C] text-center md:text-left">
+              <span className="font-black">Hensell </span>
+              is a Full Stack Developer creating seamless web and mobile
+              experiences with modern technologies
+            </h1>
+          </div>
         </div>
 
-        <div className="flex w-full max-w-[850px] items-start justify-center text-black bg-[#EDEBE6] p-5 md:p-16 rounded-3xl shadow-lg">
+        <div className=" text-black bg-[#EDEBE6] p-5 md:p-16 rounded-3xl shadow-lg md:col-span-1 md:row-span-1">
           <div className="w-full">
             <Accordion>
               {questions.map((e, i) => {
@@ -33,6 +35,9 @@ export default function Home() {
             </Accordion>
           </div>
         </div>
+        <div className="bg-[#EDEBE6] p-5 md:p-16 rounded-3xl shadow-lg md:col-span-1 md:row-span-1 h-96">
+            Hola
+          </div>
       </section>
 
       <section
@@ -53,12 +58,23 @@ export default function Home() {
 
 const questions = [
   {
-    question: "3 años de experiencia",
-    answer: `Actualmente trabajando en FDL y quantix!`,
+    question: "How much experience do you have?",
+    answer:
+      "I have 3 years of experience in software development, currently working at FDL and Quantix.",
   },
-  { question: "Mis tecnologias preferidas", answer: "Flutter, react y más" },
   {
-    question: "Poner otra cosa",
-    answer: "Eso",
+    question: "What are your preferred technologies?",
+    answer: "React, Next.js, Flutter, and Node.js (with Express and NestJS).",
+  },
+
+  {
+    question: "What projects have you worked on?",
+    answer:
+      "I have built a LinkTree-inspired app with React and TypeScript, created an offline map for client management in Flutter, and developed an internal app integrating OpenAI APIs for support teams.",
+  },
+  {
+    question: "What is your favorite aspect of programming?",
+    answer:
+      "The challenge of solving problems creatively and building applications that make people's lives easier.",
   },
 ];
