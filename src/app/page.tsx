f5d07071b72b20a58687406be1270c3d594b2d8e
Hensell <hensell04@hotmail.com>
2025-01-12 23:07:31 -0600

    Better h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,12 @@ export default function Home() {
         className="scroll-mt-[80px] flex flex-col lg:flex-row gap-4 mt-8"
       >
         <div className="bg-card p-5 md:p-16 rounded-3xl shadow-sm mr-0 md:mr-5  w-full lg:w-[85%]">
-          <h1 className="text-[clamp(1rem,calc(2vw+0.8rem),3.5rem)]  text-center lg:text-left">
-            <span className="font-black">Hensell </span>
-            is a Full Stack Developer creating seamless web and mobile
-            experiences with modern technologies
-          </h1>
+        <h1 className="text-[clamp(1rem,calc(1.8vw+0.8rem),3.5rem)] text-center lg:text-left">
+  <span className="font-black">Hi, I’m Hensell! </span>
+  I’m a passionate Full Stack Developer creating amazing web and mobile
+  experiences with cutting-edge technologies.
+</h1>
+
         </div>
 
         <dl className="bg-card p-5 md:p-16 rounded-3xl shadow-sm">
@@ -48,21 +49,21 @@ export default function Home() {
 
 const questions = [
   {
-    question: "How much experience do you have?",
+    question: "How much experience do I have?",
     answer:
-      "I have 3 years of experience in software development, currently working at FDL and Quantix.",
+      "I have 3 years of experience in software development. Currently, I work at FDL and also freelance for Quantix.",
   },
   {
-    question: "What are your preferred technologies?",
+    question: "What are my preferred technologies?",
     answer: "React, Next.js, Flutter, and Node.js (with Express and NestJS).",
   },
   {
-    question: "What projects have you worked on?",
+    question: "What projects have I worked on?",
     answer:
       "I have built a LinkTree-inspired app with React and TypeScript, created an offline map for client management in Flutter, and developed an internal app integrating OpenAI APIs for support teams.",
   },
   {
-    question: "What is your favorite aspect of programming?",
+    question: "What is my favorite aspect of programming?",
     answer:
       "The challenge of solving problems creatively and building applications that make people's lives easier.",
   },
