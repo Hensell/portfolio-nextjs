bda3d2f3bb0403d8d72fba1c2cdfdd59ede8baac
Hensell <hensell04@hotmail.com>
2025-01-19 15:28:50 -0600

    Fix bugs

--- src/app/components/drawer.tsx
+++ src/app/components/drawer.tsx
@@ -16,47 +16,6 @@ import {
 } from "@/components/ui/drawer";
 
 
-const data = [
-  {
-    goal: 400,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 278,
-  },
-  {
-    goal: 189,
-  },
-  {
-    goal: 239,
-  },
-  {
-    goal: 300,
-  },
-  {
-    goal: 200,
-  },
-  {
-    goal: 278,
-  },
-  {
-    goal: 189,
-  },
-  {
-    goal: 349,
-  },
-];
 
 export function DrawerProjects() {
   const [goal, setGoal] = React.useState(350);
