bc33083cbd9c4ee388f040ffc9a475ba5b78a504
Hensell <hensell04@hotmail.com>
2025-02-09 12:25:06 -0600

    Update all

---
+++ src/app/components/ui/DrawerComponent.tsx
@@ -0,0 +1,64 @@
+import {
+  Drawer,
+  DrawerContent,
+  DrawerBody,
+  DrawerFooter,
+  Button,
+  useDisclosure,
+  Link,
+} from "@heroui/react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+export default function App() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <div className="sm:hidden">
+      <Button onPress={onOpen} isIconOnly variant="light" radius="none">
+        <Icon icon="gridicons:menu" />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="left"
+        radius="none"
+        backdrop="blur"
+        size="xs"
+        motionProps={{
+          variants: {
+            enter: {
+              opacity: 1,
+              x: 0,
+            },
+            exit: {
+              x: 100,
+              opacity: 0,
+            },
+          },
+        }}
+      >
+        <DrawerContent>
+          {(onClose) => (
+            <>
+              <DrawerBody>
+                <Link
+                  onPress={onClose}
+                  className="font-robotoSerif "
+                  color="foreground"
+                  href="#"
+                >
+                  About
+                </Link>
+              </DrawerBody>
+              <DrawerFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+              </DrawerFooter>
+            </>
+          )}
+        </DrawerContent>
+      </Drawer>
+    </div>
+  );
+}
--- src/app/components/ui/NavbarComponent.tsx
+++ src/app/components/ui/NavbarComponent.tsx
@@ -5,40 +5,17 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
   Link,
 } from "@heroui/react";
 import ToggleTheme from "./ToggleTheme";
-
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+import DrawerComponent from "./DrawerComponent";
 
 export default function App() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = ["About"];
-
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen} isMenuOpen={isMenuOpen}>
+    <Navbar>
       <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
         <NavbarBrand>
-          <AcmeLogo />
+          <DrawerComponent />
           <p className="font-robotoSerif font-bold text-inherit">HENSELL</p>
         </NavbarBrand>
       </NavbarContent>
@@ -55,21 +32,6 @@ export default function App() {
           <ToggleTheme />
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              onPress={() => setIsMenuOpen(false)}
-              color="foreground"
-              className="w-full"
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
     </Navbar>
   );
 }
