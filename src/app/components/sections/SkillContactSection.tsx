55c40e6982050516e3fe38f252e85552747503a1
Hensell <Hensell04@hotmail.com>
2025-02-18 22:54:33 -0600

    Update SkillContactSection.tsx

--- src/app/components/sections/SkillContactSection.tsx
+++ src/app/components/sections/SkillContactSection.tsx
@@ -11,21 +11,7 @@ export default function App() {
 
   return (
     <section className="flex flex-row  max-w-screen-md mx-auto justify-between mb-40 font-robotoSerif px-12 text-xs">
-      <div>
-        <h2 className="opacity-85 tracking-widest mb-10">CONTACT</h2>
-        <div className="flex flex-col gap-3">
-          <LinkComponent
-            url={"https://www.linkedin.com/in/hensell-espinoza-a2b473125/"}
-            label={"LINKEDIN"}
-          />
-          <LinkComponent label={"RESUME"} />
-          <LinkComponent url={"https://github.com/Hensell"} label={"GITHUB"} />
-          <LinkComponent
-            onPress={() => copyToClipboard("hensell104@gmail.com")}
-            label={"EMAIL"}
-          />
-        </div>
-      </div>
+      
       <div>
         <h2 className="opacity-85 tracking-widest mb-10">SKILLS & FOCUS</h2>
         <div className="flex flex-col gap-3">
@@ -38,6 +24,21 @@ export default function App() {
 
           <LinkComponent url="https://flutter.dev/" label="FLUTTER" />
           <LinkComponent url="https://nextjs.org/" label="NEXT JS" />
+        </div>
+      </div>
+<div>
+        <h2 className="opacity-85 tracking-widest mb-10">CONTACT</h2>
+        <div className="flex flex-col gap-3">
+          <LinkComponent
+            url={"https://www.linkedin.com/in/hensell-espinoza-a2b473125/"}
+            label={"LINKEDIN"}
+          />
+          <LinkComponent label={"RESUME"} />
+          <LinkComponent url={"https://github.com/Hensell"} label={"GITHUB"} />
+          <LinkComponent
+            onPress={() => copyToClipboard("hensell104@gmail.com")}
+            label={"EMAIL"}
+          />
         </div>
       </div>
     </section>
