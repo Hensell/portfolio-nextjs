64018650c5c7f144bc2bf2124aa5edf5a409f0b3
Hensell <hensell04@hotmail.com>
2025-02-19 20:46:30 -0600

    Update SkillContactSection

--- src/app/components/sections/SkillContactSection.tsx
+++ src/app/components/sections/SkillContactSection.tsx
@@ -10,35 +10,73 @@ export default function App() {
   };
 
   return (
-    <section className="flex flex-row  max-w-screen-md justify-between mb-80 font-robotoSerif px-12 text-xs">
-      
-      <div>
-        <h2 className="opacity-85 tracking-widest mb-10">SKILLS & FOCUS</h2>
-        <div className="flex flex-col gap-3">
-          <LinkComponent
-            url="https://nestjs.com/"
-            label={"NODE JS (NEST JS)"}
-          />
+    <section className="flex flex-col  max-w-screen-md mb-52 font-robotoSerif px-12 text-xs">
+      <div className="flex max-w-screen-md justify-between mb-20">
+        <div>
+          <h2 className="opacity-85 tracking-widest mb-10">SKILLS & FOCUS</h2>
+          <div className="flex flex-col gap-3">
+            <LinkComponent
+              url="https://nestjs.com/"
+              label={"NODE JS (NEST JS)"}
+            />
 
-          <LinkComponent url="https://expressjs.com/" label="EXPRESS JS" />
+            <LinkComponent url="https://expressjs.com/" label="EXPRESS JS" />
 
-          <LinkComponent url="https://flutter.dev/" label="FLUTTER" />
-          <LinkComponent url="https://nextjs.org/" label="NEXT JS" />
+            <LinkComponent url="https://flutter.dev/" label="FLUTTER" />
+            <LinkComponent url="https://nextjs.org/" label="NEXT JS" />
+          </div>
+        </div>
+        <div>
+          <h2 className="opacity-85 tracking-widest mb-10">CONTACT</h2>
+          <div className="flex flex-col gap-3">
+            <LinkComponent
+              url={"https://www.linkedin.com/in/hensell-espinoza-a2b473125/"}
+              label={"LINKEDIN"}
+            />
+            <LinkComponent label={"RESUME"} />
+            <LinkComponent
+              url={"https://github.com/Hensell"}
+              label={"GITHUB"}
+            />
+            <LinkComponent
+              onPress={() => copyToClipboard("hensell104@gmail.com")}
+              label={"EMAIL"}
+            />
+          </div>
         </div>
       </div>
-<div>
-        <h2 className="opacity-85 tracking-widest mb-10">CONTACT</h2>
-        <div className="flex flex-col gap-3">
-          <LinkComponent
-            url={"https://www.linkedin.com/in/hensell-espinoza-a2b473125/"}
-            label={"LINKEDIN"}
-          />
-          <LinkComponent label={"RESUME"} />
-          <LinkComponent url={"https://github.com/Hensell"} label={"GITHUB"} />
-          <LinkComponent
-            onPress={() => copyToClipboard("hensell104@gmail.com")}
-            label={"EMAIL"}
-          />
+      <div className="flex max-w-screen-md justify-between">
+        <div>
+          <h2 className="opacity-85 tracking-widest mb-10">SKILLS & FOCUS</h2>
+          <div className="flex flex-col gap-3">
+            <LinkComponent
+              url="https://nestjs.com/"
+              label={"NODE JS (NEST JS)"}
+            />
+
+            <LinkComponent url="https://expressjs.com/" label="EXPRESS JS" />
+
+            <LinkComponent url="https://flutter.dev/" label="FLUTTER" />
+            <LinkComponent url="https://nextjs.org/" label="NEXT JS" />
+          </div>
+        </div>
+        <div>
+          <h2 className="opacity-85 tracking-widest mb-10">CONTACT</h2>
+          <div className="flex flex-col gap-3">
+            <LinkComponent
+              url={"https://www.linkedin.com/in/hensell-espinoza-a2b473125/"}
+              label={"LINKEDIN"}
+            />
+            <LinkComponent label={"RESUME"} />
+            <LinkComponent
+              url={"https://github.com/Hensell"}
+              label={"GITHUB"}
+            />
+            <LinkComponent
+              onPress={() => copyToClipboard("hensell104@gmail.com")}
+              label={"EMAIL"}
+            />
+          </div>
         </div>
       </div>
     </section>
