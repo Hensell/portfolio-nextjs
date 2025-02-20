7a6cfe5b3db4d231fd6043f0be128bf311370656
Hensell <hensell04@hotmail.com>
2025-02-19 20:38:47 -0600

    Test commit

--- src/app/components/sections/ProjectCardSection.tsx
+++ src/app/components/sections/ProjectCardSection.tsx
@@ -5,13 +5,11 @@ export default function App() {
     <section className="grid grid-cols-1 sm:grid-cols-2 w-screen h-auto bg-secondarybg">
       {[...Array(4)].map((_, index) => (
         <div key={index} className="relative w-full h-64 overflow-hidden group">
-          {/* Contenedor del Texto (efecto sobre) */}
           <div className="font-robotoSerif absolute inset-0 bg-secondarybg flex flex-col items-center justify-center transition-transform duration-500 ease-in-out group-hover:scale-y-0 origin-bottom">
             <h3 className="text-xl font-bold mb-2">QUANTIX {index + 1}</h3>
             <p className="text-sm">QUANTIX WEBSITE {index + 1}</p>
           </div>
 
-          {/* Imagen con animación deslizante */}
           <Image
             src={`/projects/quantixPage.png`}
             alt={`project ${index + 1}`}
