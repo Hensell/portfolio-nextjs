7f4a269acb55eedad59066319b4e6fc139662bcd
Hensell <hensell04@hotmail.com>
2025-03-19 22:00:12 -0600

    Update about me

---
+++ src/app/components/sections/MeSection.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function App() {
+  return (
+    <div className="px-12 font-robotoSerif space-y-2">
+      <h2>ABOUT ME</h2>
+      <hr />
+      <p className="text-xs font-thin">
+        Results-driven Full-Stack Developer with 3+ years of experience in web
+        and mobile app development, specializing in Next.js, Flutter,
+        Express.js, NestJS, and Android with Jetpack Compose. Skilled in backend
+        optimization, RESTful APIs, GraphQL, database management (PostgreSQL,
+        Firebase, MongoDB), and test automation. Passionate about writing clean,
+        maintainable, and scalable code to build high-performance applications.
+        Experienced in remote collaboration, Agile methodologies (Scrum,
+        Kanban), and working with cross-functional teams. Strong problem-solving
+        skills with a focus on performance optimization, security, and DevOps
+        (Docker, CI/CD, AWS, Vercel, Firebase).
+      </p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "sonner";
 import { MainLayout } from "./components/layout/MainLayout";
 import AboutSection from "./components/sections/AboutSection";
 import SkillContactSection from "./components/sections/SkillContactSection";
+import MeSection from "./components/sections/MeSection";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
             "rounded-none bg-background text-foreground font-robotoSerif ",
         }}
       />
+      <MeSection />
       <SkillContactSection />
     </MainLayout>
   );
