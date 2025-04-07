d48b9744254adab403fe634ebf4612d1127d6137
Hensell <hensell04@hotmail.com>
2025-04-06 23:08:54 -0600

    Update about section

--- src/app/components/sections/MeSection.tsx
+++ src/app/components/sections/MeSection.tsx
@@ -6,11 +6,11 @@ export default function App() {
       <h2 className="font-robotoSerif">ABOUT ME</h2>
       <hr />
       <p className="font-roboto text-xs font-thin">
-        I’m a Full Stack Developer specialized in React, Next.js, and Node.js.
-        I’ve worked on projects for startups and companies, building scalable
-        and efficient solutions. Currently, I’m working on a wide range of
-        projects, from web applications to automation tools and custom
-        solutions.
+        I’m a Full Stack Developer specialized in Flutter, Kotlin, React,
+        Next.js, and Node.js. I’ve worked on projects for startups and
+        companies, building scalable and efficient solutions. Currently, I’m
+        working on a wide range of projects, from web applications to automation
+        tools and custom solutions.
       </p>
     </div>
   );
