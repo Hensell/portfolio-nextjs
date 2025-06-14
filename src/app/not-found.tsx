62aa9b3b3a2c112956c95e2033a446f00c91f8fc
Hensell <hensell04@hotmail.com>
2025-06-14 16:25:15 -0600

    Update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <main style={{ textAlign: "center", padding: "4rem" }}>
       <h1>404 – Page Not Found</h1>
       <p>Sorry, the page you are looking for doesn’t exist.</p>
+      <button
+        onClick={() => router.push("/")}
+        className="mt-20 inline-block px-6 py-3 rounded-md bg-foreground text-background text-sm tracking-widest hover:bg-foreground/90 transition"
+      >
+        Go back home
+      </button>
     </main>
   );
 }
