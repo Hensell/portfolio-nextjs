6e72653c14b28f6195e36dd0eca20093d08937e2
Hensell <hensell04@hotmail.com>
2025-11-29 01:54:05 -0600

    feat: update privacy policy to no data collection

--- src/app/apps/froghappy/privacy/page.tsx
+++ src/app/apps/froghappy/privacy/page.tsx
@@ -11,26 +11,14 @@ export default function PrivacyPolicy() {
             <div className="space-y-6">
                 <section>
                     <p>
-                        Hensell Espinoza built the &quot;Bullet journal - frog happy&quot; app as a
-                        Free app. This SERVICE is provided by Hensell Espinoza at no cost
-                        and is intended for use as is.
+                        Hensell Espinoza built the &quot;Bullet journal - frog happy&quot;
+                        app as a Free app. This SERVICE is provided by Hensell Espinoza at
+                        no cost and is intended for use as is.
                     </p>
                     <p className="mt-2">
                         This page is used to inform visitors regarding my policies with the
                         collection, use, and disclosure of Personal Information if anyone
                         decided to use my Service.
-                    </p>
-                    <p className="mt-2">
-                        If you choose to use my Service, then you agree to the collection and
-                        use of information in relation to this policy. The Personal
-                        Information that I collect is used for providing and improving the
-                        Service. I will not use or share your information with anyone except
-                        as described in this Privacy Policy.
-                    </p>
-                    <p className="mt-2">
-                        The terms used in this Privacy Policy have the same meanings as in
-                        our Terms and Conditions, which is accessible at &quot;Bullet journal -
-                        frog happy&quot; unless otherwise defined in this Privacy Policy.
                     </p>
                 </section>
 
@@ -39,104 +27,57 @@ export default function PrivacyPolicy() {
                         Information Collection and Use
                     </h2>
                     <p>
-                        For a better experience, while using our Service, I may require you
-                        to provide us with certain personally identifiable information. The
-                        information that I request will be retained on your device and is
-                        not collected by me in any way.
+                        I do not collect, use, save, or have access to any of your personal
+                        data recorded in &quot;Bullet journal - frog happy&quot;.
                     </p>
                     <p className="mt-2">
-                        The app does use third party services that may collect information
-                        used to identify you.
+                        Individual settings and data are stored only on your device. You can
+                        delete all data stored by the app by clearing the app&apos;s data in
+                        your device settings or by uninstalling the app.
                     </p>
-                    <p className="mt-2">
-                        Link to privacy policy of third party service providers used by the
-                        app:
-                    </p>
-                    <ul className="list-disc list-inside ml-4 mt-2">
-                        <li>
-                            <a
-                                href="https://www.google.com/policies/privacy/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-primary hover:underline"
-                            >
-                                Google Play Services
-                            </a>
-                        </li>
-                    </ul>
                 </section>
 
                 <section>
                     <h2 className="text-xl font-semibold mb-2">Log Data</h2>
                     <p>
-                        I want to inform you that whenever you use my Service, in a case of
-                        an error in the app I collect data and information (through third
-                        party products) on your phone called Log Data. This Log Data may
-                        include information such as your device Internet Protocol (“IP”)
-                        address, device name, operating system version, the configuration of
-                        the app when utilizing my Service, the time and date of your use of
-                        the Service, and other statistics.
+                        I want to inform you that whenever you use my Service, I do not
+                        collect any data or information on your phone called Log Data.
                     </p>
                 </section>
 
                 <section>
                     <h2 className="text-xl font-semibold mb-2">Cookies</h2>
                     <p>
-                        Cookies are files with a small amount of data that are commonly used
-                        as anonymous unique identifiers. These are sent to your browser from
-                        the websites that you visit and are stored on your device&apos;s internal
-                        memory.
-                    </p>
-                    <p className="mt-2">
-                        This Service does not use these “cookies” explicitly. However, the
-                        app may use third party code and libraries that use “cookies” to
-                        collect information and improve their services. You have the option
-                        to either accept or refuse these cookies and know when a cookie is
-                        being sent to your device. If you choose to refuse our cookies, you
-                        may not be able to use some portions of this Service.
+                        This Service does not use &quot;cookies&quot;. The app does not use
+                        any third party code or libraries that use &quot;cookies&quot; to
+                        collect information.
                     </p>
                 </section>
 
                 <section>
                     <h2 className="text-xl font-semibold mb-2">Service Providers</h2>
                     <p>
-                        I may employ third-party companies and individuals due to the
-                        following reasons:
-                    </p>
-                    <ul className="list-disc list-inside ml-4 mt-2">
-                        <li>To facilitate our Service;</li>
-                        <li>To provide the Service on our behalf;</li>
-                        <li>To perform Service-related services; or</li>
-                        <li>To assist us in analyzing how our Service is used.</li>
-                    </ul>
-                    <p className="mt-2">
-                        I want to inform users of this Service that these third parties have
-                        access to your Personal Information. The reason is to perform the
-                        tasks assigned to them on our behalf. However, they are obligated
-                        not to disclose or use the information for any other purpose.
+                        I do not employ third-party companies or individuals to facilitate
+                        my Service, to provide the Service on my behalf, to perform
+                        Service-related services, or to assist me in analyzing how my
+                        Service is used.
                     </p>
                 </section>
 
                 <section>
                     <h2 className="text-xl font-semibold mb-2">Security</h2>
                     <p>
-                        I value your trust in providing us your Personal Information, thus
-                        we are striving to use commercially acceptable means of protecting
-                        it. But remember that no method of transmission over the internet,
-                        or method of electronic storage is 100% secure and reliable, and I
-                        cannot guarantee its absolute security.
+                        Since I do not collect any Personal Information, you do not need to
+                        worry about the security of your data in transit or storage on my
+                        servers, as no such transmission or storage occurs. Your data
+                        remains strictly on your device.
                     </p>
                 </section>
 
                 <section>
                     <h2 className="text-xl font-semibold mb-2">Links to Other Sites</h2>
                     <p>
-                        This Service may contain links to other sites. If you click on a
-                        third-party link, you will be directed to that site. Note that these
-                        external sites are not operated by me. Therefore, I strongly advise
-                        you to review the Privacy Policy of these websites. I have no
-                        control over and assume no responsibility for the content, privacy
-                        policies, or practices of any third-party sites or services.
+                        This Service does not contain links to other sites.
                     </p>
                 </section>
 
@@ -145,11 +86,7 @@ export default function PrivacyPolicy() {
                     <p>
                         These Services do not address anyone under the age of 13. I do not
                         knowingly collect personally identifiable information from children
-                        under 13. In the case I discover that a child under 13 has provided
-                        me with personal information, I immediately delete this from our
-                        servers. If you are a parent or guardian and you are aware that your
-                        child has provided us with personal information, please contact me
-                        so that I will be able to do necessary actions.
+                        under 13.
                     </p>
                 </section>
 
